611eafe58405fb3d0abfc548373e4ecb35e636d0
ndelvalle <nicolas.delvalle@gmail.com>
2019-09-30 17:46:28 +0900

    Update production dependencies

--- src/main/index.js
+++ src/main/index.js
@@ -4,13 +4,13 @@ const path = require('path')
 const { menubar } = require('menubar')
 const { ipcMain } = require('electron')
 
-const isDev = process.env.NODE_ENV === 'development'
+const IS_DEV = process.env.NODE_ENV === 'development'
 
 /**
  * Set `__static` path to static files in production
  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
  */
-if (!isDev) {
+if (!IS_DEV) {
   global.__static = path.join(__dirname, '/static').replace(/\\/g, '\\\\')
 }
 
@@ -21,15 +21,15 @@ const mb = menubar({
 
   browserWindow: {
     transparent: true,
-    resizable: isDev,
-    movable: isDev,
+    resizable: IS_DEV,
+    movable: IS_DEV,
     width: 346,
     height: 272
   },
 
   // Note: App icon downloaded from https://icons8.com/icon/1935/display
   // Note: App trail icon downloaded from https://icons8.com/icon/1935/display
-  icon: path.join(isDev ? process.cwd() : __dirname, '/static/favicon/icon.png'),
+  icon: path.join(IS_DEV ? process.cwd() : __dirname, '/static/favicon/icon.png'),
   index: process.env.NODE_ENV === 'development'
     ? `http://localhost:9080`
     : `file://${__dirname}/index.html`
--- src/renderer/services/spoof.js
+++ src/renderer/services/spoof.js
@@ -1,19 +1,23 @@
+import path from 'path'
 import spoof from 'spoof'
 import exec from './sudo-prompt'
 
-const SPOOF_BIN_PATH = './node_modules/.bin/spoof '
+const IS_DEV = process.env.NODE_ENV === 'development'
+const SPOOF_BIN_PATH = IS_DEV
+  ? './node_modules/.bin/spoof'
+  : path.resolve(process.resourcesPath, 'spoof')
 
 function isSpoofed (target) {
   return target.currentAddress && target.currentAddress !== target.address
 }
 
 function randomize (device) {
-  const cmd = `randomize ${device}`
+  const cmd = ` randomize ${device}`
   return exec(SPOOF_BIN_PATH + cmd)
 }
 
 function reset (device) {
-  const cmd = `reset ${device}`
+  const cmd = ` reset ${device}`
   return exec(SPOOF_BIN_PATH + cmd)
 }
 
@@ -21,7 +25,7 @@ function resetAll (devices) {
   if (!devices) {
     return
   }
-  const cmd = 'reset '
+  const cmd = ' reset '
   return exec(SPOOF_BIN_PATH + cmd + devices)
 }
 
